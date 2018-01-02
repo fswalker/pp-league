529b9975e9b4d768972e67d0d3f81d15f0779fef
Michał Grygierzec <michal.grygierzec@gmail.com>
2018-01-02 01:01:50 +0100

    Basic structure of web app

---
+++ README.md
@@ -0,0 +1,4 @@
+# ping-pong league
+
+_ping-pong league_ is a web application which goal is to help to track and manage ping-pong league tournaments in the most approachable, easy and user-friendly way.
+
---
+++ src/elm/Main.elm
@@ -0,0 +1,70 @@
+port module Main exposing (..)
+
+import Html exposing (Html, div, h1, text)
+import Html.Attributes exposing (class)
+import Html.Events exposing (onClick)
+
+
+type User
+    = Anonymous
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { user : User
+    }
+
+
+model : Model
+model =
+    { user = Anonymous
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( model, Cmd.none )
+
+
+
+-- PORTS
+-- UPDATE
+
+
+type Msg
+    = String
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h1 []
+            [ text "ping-pong league" ]
+        ]
+
+
+subscriptions : Model -> Sub Msg
+subscriptions =
+    \_ -> Sub.none
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
