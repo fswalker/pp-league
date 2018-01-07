c5425872b78c6f1a25f06263c0c94c6f46a4c981
Michał Grygierzec <michal.grygierzec@gmail.com>
2018-01-07 12:02:00 +0100

    Refactor page view, add _mixins.sass

--- src/elm/Views/Page.elm
+++ src/elm/Views/Page.elm
@@ -10,58 +10,85 @@ frame : Html msg
 frame =
     section [ class "page-frame" ]
         [ div [ class "container" ]
-            [ header [ class "header" ]
-                [ div [ class "container" ]
-                    [ div [ class "level" ]
-                        [ div [ class "level-left" ]
-                            [ h1 [ class "title is-size-2" ]
-                                [ text "Ping-pong League" ]
-                            ]
-                        , div [ class "level-right greeting" ]
-                            [ h3 [ class "subtitle" ]
-                                [ text "Hello, "
-                                , strong [] [ text "Guest" ]
-                                , text "!"
+            [ ppHeader
+            , navigation
+            , div [ class "page-content" ]
+                [ p [] [ text "TODO" ]
+                ]
+            , ppFooter
+            ]
+        ]
+
+
+ppHeader : Html msg
+ppHeader =
+    header [ class "header" ]
+        [ div [ class "container" ]
+            [ div [ class "level" ]
+                [ div [ class "level-left" ]
+                    [ a [ href "#", title "Link - Home Page" ]
+                        [ div [ class "level" ]
+                            [ div [ class "level-left" ]
+                                [ img
+                                    [ class "level-item logo image is-128x128"
+                                    , Assets.src Assets.pp_menu
+                                    , alt "Logo image - racket and the ball"
+                                    ]
+                                    []
+                                , h1 [ class "page-title level-item title is-size-2" ]
+                                    [ text "Ping-pong League" ]
                                 ]
                             ]
                         ]
                     ]
-                ]
-            , nav
-                [ class "navbar is-transparent main"
-                , attribute "role" "navigation"
-                , attribute "aria-label" "main navigation"
-                ]
-                [ div [ class "navbar-brand" ]
-                    [ -- [ a [ class "navbar-item", href "#" ]
-                      --     [ img [ Assets.src Assets.pp_menu ] []
-                      --     ]
-                      div [ class "button navbar-burger burger", attribute "data-target" "pp-navbar-menu" ]
-                        [ span [] []
-                        , span [] []
-                        , span [] []
-                        ]
-                    ]
-                , div [ class "navbar-menu", id "pp-navbar-menu" ]
-                    [ div [ class "navbar-start is-size-5" ]
-                        [ a [ class "navbar-item", href "#" ] [ text "Home" ]
+                , div [ class "level-right greeting" ]
+                    [ h3 [ class "subtitle" ]
+                        [ text "Hello, "
+                        , strong [] [ text "Guest" ]
+                        , text "!"
                         ]
                     ]
                 ]
-            , div [ class "page-content" ]
-                [ p [] [ text "TODO" ]
+            ]
+        ]
+
+
+navigation : Html msg
+navigation =
+    nav
+        [ class "navbar is-transparent main"
+        , attribute "role" "navigation"
+        , attribute "aria-label" "main navigation"
+        ]
+        [ div [ class "navbar-brand" ]
+            [ -- [ a [ class "navbar-item", href "#" ]
+              --     [ img [ Assets.src Assets.pp_menu ] []
+              --     ]
+              div [ class "button navbar-burger burger", attribute "data-target" "pp-navbar-menu" ]
+                [ span [] []
+                , span [] []
+                , span [] []
                 ]
-            , footer [ class "footer is-paddingless" ]
-                [ div [ class "container" ]
-                    [ div [ class "content" ]
-                        [ p []
-                            [ strong [] [ text "Ping-pong League " ]
-                            , text "web app. "
-                            , text "The source code is licensed "
-                            , span [] [ text "MIT. " ]
-                            , text ((String.fromChar <| Char.fromCode 169) ++ " Michał Grygierzec 2018.")
-                            ]
-                        ]
+            ]
+        , div [ class "navbar-menu", id "pp-navbar-menu" ]
+            [ div [ class "navbar-start is-size-5" ]
+                [ a [ class "navbar-item", href "#", title "Link - Home Page" ] [ text "Home" ]
+                ]
+            ]
+        ]
+
+
+ppFooter : Html msg
+ppFooter =
+    footer [ class "footer is-paddingless" ]
+        [ div [ class "container" ]
+            [ div [ class "content" ]
+                [ p []
+                    [ strong [] [ text "Ping-pong League " ]
+                    , text "web app. "
+                    , text "The source code is licensed "
+                    , span [] [ text "MIT. " ]
+                    , text ((String.fromChar <| Char.fromCode 169) ++ " Michał Grygierzec 2018.")
                     ]
                 ]
             ]
